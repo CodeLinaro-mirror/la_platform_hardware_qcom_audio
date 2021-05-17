36def08ec757d43932f9cedacc804271055e1b56
Kishan Kumar Majeti <kkmajeti@codeaurora.org>
2021-05-17 14:58:26 +0530

    audiohal: Add support for mono volume
    Change-Id: I6e7f8003958c7c7cd8837e7d07e3ae7c599ddbe3
    Signed-off-by: Meghana Reddy Mula <mmula@codeaurora.org>

--- qahw_api/src/qahw_api.cpp
+++ qahw_api/src/qahw_api.cpp
@@ -2622,9 +2622,18 @@ int qahw_stream_set_volume(qahw_stream_handle_t *stream_handle,
                   right channels\n", __func__);
         }
     } /*currently HAL requires 2 channels only */
-    else if (vol_data.num_of_channels == QAHW_CHANNELS_MAX &&
-               vol_data.vol_pair) {
-        if((l_found && r_found) && (left == right)) {
+    else if (vol_data.vol_pair) {
+        if((l_found  == true)&& (r_found == false))
+        {
+            right = left;
+        }
+
+        if((l_found  == false)&& (r_found == true))
+        {
+            left = right;
+        }
+
+        if(left == right) {
             switch (stream->dir) {
             case QAHW_STREAM_INPUT:
                 rc = qahw_in_set_volume(stream->in_stream,
@@ -2640,6 +2649,8 @@ int qahw_stream_set_volume(qahw_stream_handle_t *stream_handle,
                 for(i=0; i < vol_data.num_of_channels; i++) {
                     stream->vol.vol_pair[i] = vol_data.vol_pair[i];
                 }
+
+                stream->vol.num_of_channels = vol_data.num_of_channels;
             }
         } else
             ALOGE("%s: vol setting requires equal value for both left and \
