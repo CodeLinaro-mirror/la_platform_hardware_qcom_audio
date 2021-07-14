a477d8f4f6c7ed41fa312140004df4d6c40c4cf6
Sujin Panicker <spanic@codeaurora.org>
2021-07-14 15:04:43 +0530

    hal: Fix CFI errors
    CFI check is failing due to difference in
    function signature during CVE-2019-10581
    POC test. Fix the issue by matching the
    function signature in header file and
    function pointer type.
    
    Change-Id: I92ccb0871b09f5757195844984519d51367ed35f

--- hal/audio_extn/battery_listener.h
+++ hal/audio_extn/battery_listener.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2018, The Linux Foundation. All rights reserved.
+* Copyright (c) 2018, 2021 The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
@@ -32,13 +32,13 @@ extern "C" {
 #endif
 typedef void (* battery_status_change_fn_t)(bool);
 void audio_extn_battery_properties_listener_init(battery_status_change_fn_t fn);
-void audio_extn_battery_properties_listener_deinit();
-bool audio_extn_battery_properties_is_charging();
+void audio_extn_battery_properties_listener_deinit(void);
+bool audio_extn_battery_properties_is_charging(void);
 #ifdef __cplusplus
 }
 #endif
 #else
 #define audio_extn_battery_properties_listener_init(fn) do { } while(0)
-#define audio_extn_battery_properties_listener_deinit() do { } while(0)
-#define audio_extn_battery_properties_is_charging() (false)
+#define audio_extn_battery_properties_listener_deinit(void) do { } while(0)
+#define audio_extn_battery_properties_is_charging(void) (false)
 #endif
