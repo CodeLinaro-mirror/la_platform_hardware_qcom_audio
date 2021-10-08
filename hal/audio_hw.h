388bc7289b972df0bbcdede27685b1a58dbb148c
Tahir Dawson <dawson@codeaurora.org>
2021-10-07 20:42:04 -0400

    hal: Add support to close streams on power policy callback
    * Close existing input and output streams during power policy disable callback
    
    * Stop new streams from starting if current policy is in disabled state
    
    Change-Id: I11f7ea1bad1022f86e918e25d71a7e5bca2e70d5
    Signed-off-by: Tahir Dawson <dawson@codeaurora.org>

--- hal/audio_hw.h
+++ hal/audio_hw.h
@@ -159,6 +159,10 @@ typedef enum card_status_t {
     CARD_STATUS_ONLINE
 } card_status_t;
 
+typedef enum power_policy_status_t {
+    POWER_POLICY_STATUS_OFFLINE,
+    POWER_POLICY_STATUS_ONLINE
+} power_policy_status_t;
 /* These are the supported use cases by the hardware.
  * Each usecase is mapped to a specific PCM device.
  * Refer to pcm_device_table[].
@@ -715,6 +719,8 @@ struct audio_device {
 
     int snd_card;
     card_status_t card_status;
+    power_policy_status_t out_power_policy;
+    power_policy_status_t in_power_policy;
     unsigned int cur_codec_backend_samplerate;
     unsigned int cur_codec_backend_bit_width;
     bool is_channel_status_set;
@@ -800,6 +806,9 @@ struct audio_patch_record {
     audio_usecase_t usecase;
     struct audio_patch patch;
 };
+
+void out_set_power_policy(uint8_t enable);
+void in_set_power_policy(uint8_t enable);
 
 int select_devices(struct audio_device *adev,
                           audio_usecase_t uc_id);
