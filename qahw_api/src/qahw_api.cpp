ca250cf771a314b2a07ef4250dc24682d8d0d2ca
Kishan Kumar Majeti <quic_kkmajeti@quicinc.com>
2022-01-04 12:16:46 +0530

    qahw: Fix mute and unmute parameters
    Update mute param structure with out overriding existing structure
    
    Change-Id: I84364bffa74a2eb1d12b10b218a3867b4bd43979

--- qahw_api/src/qahw_api.cpp
+++ qahw_api/src/qahw_api.cpp
@@ -25,6 +25,40 @@
 * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* Changes from Qualcomm Innovation Center are provided under the following license:
+* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted (subject to the limitations in the
+* disclaimer below) provided that the following conditions are met:
+*
+*      * Redistributions of source code must retain the above copyright
+*        notice, this list of conditions and the following disclaimer.
+*
+*      * Redistributions in binary form must reproduce the above
+*        copyright notice, this list of conditions and the following
+*        disclaimer in the documentation and/or other materials provided
+*        with the distribution.
+*
+*      * Neither the name of Qualcomm Innovation Center, Inc. nor the
+*        names of its contributors may be used to endorse or promote
+*        products derived from this software without specific prior
+*        written permission.
+*
+* NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+* GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+* HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+* IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+* OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #define LOG_TAG "qahw_api"
@@ -2746,13 +2780,16 @@ int qahw_stream_set_mute(qahw_stream_handle_t *stream_handle,
 
     ALOGV("%d:%s start",__LINE__, __func__);
     if ((stream->type == QAHW_VOICE_CALL) ||(stream->type == QAHW_ECALL)) {
-        char *mute_param;
+        char *mute_detail = NULL;
+        char mute_param[QAHW_KV_PAIR_LENGTH];
 
-        mute_param = qahw_get_device_mute_info(mute_data);
-        if (mute_param == NULL)
+        mute_detail = qahw_get_device_mute_info(mute_data);
+        if (mute_detail == NULL)
             return rc;
 
-        int mutelen =  strlen(mute_param);
+        int mutelen =  strlen(mute_detail);
+        memset((char*)mute_param, 0, QAHW_KV_PAIR_LENGTH);
+        strlcpy((char*)mute_param, mute_detail, QAHW_KV_PAIR_LENGTH);
         mute_param[mutelen] = ';';
         mute_param[mutelen + 1] = '\0';
         strlcat(mute_param,stream->sess_id_call_state, QAHW_KV_PAIR_LENGTH);
