3128f9eb76891048024a97b33208805d5a7c803e
Kishan Kumar Majeti <quic_kkmajeti@quicinc.com>
2022-06-02 09:06:40 +0530

    audiohal: Fix device switch issue for invalid device
    Fix device switch issue when invalid device is set for playback and voicecall
    
    Change-Id: I695faf10273a52e03009939f421d78376174e35f

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -4417,7 +4417,7 @@ static int out_set_parameters(struct audio_stream *stream, const char *kvpairs)
     if (err >= 0) {
         val = atoi(value);
 
-        if(out->devices & AUDIO_DEVICE_BIT_IN)
+        if(val & AUDIO_DEVICE_BIT_IN)
            goto routing_fail;
 
         lock_output_stream(out);
