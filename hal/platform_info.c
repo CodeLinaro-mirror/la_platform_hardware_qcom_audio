7eb513b70aab60032f647deec64faee643118407
Santosh Barnwal <quic_barnwal@quicinc.com>
2022-10-25 14:54:45 +0530

    Revert "hal : send audio cal using v6 api with fe id"
    This reverts commit 7951b25f1ae595c5dd30ead29e4bdfd7b3323228.
    
    Change-Id: Id880748b3dcf2505fb1d37d79f3729fa415e6082

--- hal/platform_info.c
+++ hal/platform_info.c
@@ -389,27 +389,20 @@ static void process_pcm_id(const XML_Char **attr)
         goto done;
     }
 
-    int pcm_id = atoi((char *)attr[5]);
+    int id = atoi((char *)attr[5]);
 
 #ifdef PLATFORM_AUTO
-    if (strcmp(attr[6], "fe") != 0) {
-        ALOGE("%s: fe id not mentioned", __func__);
-        goto done;
-    }
-
-    int fe_id = atoi((char *)attr[7]);
-
-    if (platform_set_usecase_pcm_id(index, type, pcm_id, fe_id) < 0) {
-        ALOGE("%s: usecase %s type %d pcm_id %d fe_id %d was not set!",
-              __func__, attr[1], type, pcm_id, fe_id);
+    if (platform_set_usecase_pcm_id(index, type, id) < 0) {
+        ALOGE("%s: usecase %s type %d id %d was not set!",
+              __func__, attr[1], type, id);
         goto done;
     }
 #else
     int fe_id = -1;
 
-    if (platform_set_usecase_pcm_id(index, type, pcm_id, fe_id) < 0) {
-        ALOGE("%s: usecase %s type %d pcm_id %d fe_id %d was not set!",
-              __func__, attr[1], type, pcm_id, fe_id);
+    if (platform_set_usecase_pcm_id(index, type, id) < 0) {
+        ALOGE("%s: usecase %s type %d id %d was not set!",
+              __func__, attr[1], type, id);
         goto done;
     }
 #endif
