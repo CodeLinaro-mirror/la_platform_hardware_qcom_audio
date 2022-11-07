0940e38d7517dc6d674c4060bd7894eb6f117b53
Kogara Naveen Kumar <quic_nkogara@quicinc.com>
2022-11-07 20:25:40 +0530

    audio-hal: Add carplay low latency concurrent playback and record usecase support
    Enable carplay low latency concurrent playback and record usecase support in audio hal
    
    Suggested-by: Shital Jadhav (Temp) <shitjadh@qti.qualcomm.com>
    
    Change-Id: Ia778aff601b5fc83deb7e873742573fde8329d16
    Signed-off-by: Kogara Naveen Kumar <quic_nkogara@quicinc.com>

--- hal/audio_extn/audio_extn.h
+++ hal/audio_extn/audio_extn.h
@@ -1482,4 +1482,5 @@ snd_device_t audio_extn_get_loopback_snd_device(struct audio_device *adev,
 
 void audio_get_vendor_config_path(char* config_file_path, int path_size);
 bool audio_extn_is_concurrent_pcm_record_enabled();
+bool audio_extn_is_concurrent_low_latency_pcm_record_enabled();
 #endif /* AUDIO_EXTN_H */
