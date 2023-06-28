c34525a3689e1b603bb2ac4bcfad710093e1d279
Manoj Kumar N D <mnd@qti.qualcomm.com>
2023-06-28 16:12:16 +0530

    audio-hal : Update data type to avoid data corruption.
    Data type is modified to size_t as it causes data corruption
    in 64 bit builds.
    
    Change-Id: I81b28aee2bd9b2abaa385b6e1481c755362344e7
    Suggested-by : Manoj Kumar N D

--- hal/audio_extn/utils.c
+++ hal/audio_extn/utils.c
@@ -1046,7 +1046,7 @@ static int set_stream_app_type_mixer_ctrl(struct audio_device *adev,
 
     char mixer_ctl_name[MAX_LENGTH_MIXER_CONTROL_IN_INT] = {0};
     struct mixer_ctl *ctl;
-    int app_type_cfg[MAX_LENGTH_MIXER_CONTROL_IN_INT] = {0}, len = 0, rc = 0;
+    size_t app_type_cfg[MAX_LENGTH_MIXER_CONTROL_IN_INT] = {0}, len = 0, rc = 0;
     int snd_device_be_idx = -1;
 
     if (stream_type == PCM_PLAYBACK) {
