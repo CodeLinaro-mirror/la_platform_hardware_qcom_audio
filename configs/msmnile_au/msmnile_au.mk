c67408dcdbac5a9f4524bf3768ae17bec6b0110c
Naveen Kumara Kogara <quic_nkogara@quicinc.com>
2023-08-10 11:10:51 +0530

    audio hal: Enable mmap path for AAudio
    Hal will try for mmap, if failed
    it will fallback for existing legacy path.
    
    Change-Id: Ica905d8783769aad3e5db04e473bfa150aacace5

--- configs/msmnile_au/msmnile_au.mk
+++ configs/msmnile_au/msmnile_au.mk
@@ -382,7 +382,7 @@ vendor.audio.hal.output.suspend.supported=false
 #Enable AAudio MMAP/NOIRQ data path
 #1 is AAUDIO_POLICY_NEVER so it will not try MMAP
 #2 is AAUDIO_POLICY_AUTO so it will try MMAP then fallback to Legacy path
-ifneq ($(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX), msmnile_gvmq)
+ifeq ($(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX), msmnile_gvmq)
 PRODUCT_PROPERTY_OVERRIDES += aaudio.mmap_policy=2
 #Allow EXCLUSIVE then fall back to SHARED.
 PRODUCT_PROPERTY_OVERRIDES += aaudio.mmap_exclusive_policy=2
