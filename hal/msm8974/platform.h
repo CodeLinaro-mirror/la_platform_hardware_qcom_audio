e10993f4f0517ac99633cd56fa0fb00dc8cd3109
Prasanna Kumar Reddy Mukka <quic_pmukka@quicinc.com>
2024-10-30 14:28:28 +0530

    audio-hal: Update to pcm config of deep buffer playback
    Adjusted period size to a power of 2, as per hal specification
    and adjusting latency to be inline with period size
    
    Change-Id: I9f377c0c12dadce8ebc4e08e65f33a9dd6095ffb
    Signed-off-by: Prasanna Kumar Reddy Mukka <pmukka@qti.qualcomm.com>

--- hal/msm8974/platform.h
+++ hal/msm8974/platform.h
@@ -470,8 +470,8 @@ enum {
  * current implementation can support buffer size of 40ms duration
  * for 32b/384Khz/stereo output.
  */
-#define DEEP_BUFFER_OUTPUT_PERIOD_SIZE 1920
-#define DEEP_BUFFER_OUTPUT_PERIOD_DURATION 40 /* 40 milisecs */
+#define DEEP_BUFFER_OUTPUT_PERIOD_SIZE 2048
+#define DEEP_BUFFER_OUTPUT_PERIOD_DURATION 42 /* 42 milisecs */
 
 #define DEEP_BUFFER_OUTPUT_PERIOD_COUNT 4
 #define LOW_LATENCY_OUTPUT_PERIOD_SIZE 240
