c992199c4c947a6aff8bf8df816803d3619aa15e
Akshaya Chirikonda <achiriko@qti.qualcomm.com>
2025-07-09 13:08:06 +0530

    qahw_api: Add check to prevent use of IN devices in output stream path
    Change-Id: I6f824606989ca8c437ea70ba56a535a7becbf83b

--- qahw_api/src/qahw_api.cpp
+++ qahw_api/src/qahw_api.cpp
@@ -27,38 +27,8 @@
 * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *
 * Changes from Qualcomm Innovation Center are provided under the following license:
-* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted (subject to the limitations in the
-* disclaimer below) provided that the following conditions are met:
-*
-*      * Redistributions of source code must retain the above copyright
-*        notice, this list of conditions and the following disclaimer.
-*
-*      * Redistributions in binary form must reproduce the above
-*        copyright notice, this list of conditions and the following
-*        disclaimer in the documentation and/or other materials provided
-*        with the distribution.
-*
-*      * Neither the name of Qualcomm Innovation Center, Inc. nor the
-*        names of its contributors may be used to endorse or promote
-*        products derived from this software without specific prior
-*        written permission.
-*
-* NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-* GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-* HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-* IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-* OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+* SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
 #define LOG_TAG "qahw_api"
@@ -2526,7 +2496,11 @@ int qahw_stream_set_device(qahw_stream_handle_t *stream_handle,
                 ALOGE("%s: invalid device params\n", __func__);
                 return rc;
             }
-
+            if (devices[0] & AUDIO_DEVICE_BIT_IN) {
+                ALOGE("%s:IN devices are not valid for output stream devices:(%#x)",
+                  __func__,devices);
+                return -EINVAL;
+            }
             snprintf(dev_s, QAHW_MAX_INT_STRING, "%u", devices[0]);
             strlcat(device_route, dev_s, QAHW_MAX_INT_STRING);
             rc = qahw_out_set_parameters(stream->out_stream,
