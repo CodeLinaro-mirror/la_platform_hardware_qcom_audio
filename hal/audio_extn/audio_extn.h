e554795df015b4cd0d0dbdaf70e188f6b43d6bc4
Vikas Dhananjaya <vdhananj@qti.qualcomm.com>
2026-01-21 15:34:50 +0530

    primary-hal: update open-source markings
    Update Open-Source Markings
    
    Change-Id: I663be362fa02632f8a6d5b70c27991c7d52a6d7f
    Signed-off-by: Vikas Dhananjaya <vdhananj@qti.qualcomm.com>

--- hal/audio_extn/audio_extn.h
+++ hal/audio_extn/audio_extn.h
@@ -34,7 +34,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
+ * ​​​​​Changes from Qualcomm Technologies, Inc. are provided under the following license:
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #ifndef AUDIO_EXTN_H
