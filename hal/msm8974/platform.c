cdeb5668c4988c64fa4263831606955ce91c0a4c
Vikas Dhananjaya <vdhananj@qti.qualcomm.com>
2026-05-20 20:34:28 +0530

    hal: Fix echo-reference not disabled on concurrent usecase
    - Guard all platform_set_echo_reference call sites with
      enable_aec, enable_ec_port to skip echo-reference setup
      for AEC-enabled streams.
    - Reset in->ec_opened flag before calling platform_set_echo_reference
      on input stream close to ensure proper cleanup.
    
    Change-Id: I8b47da582935b808996a58e65d3f2290aa4bc0f7
    Signed-off-by: Vikas Dhananjaya <vdhananj@qti.qualcomm.com>
    (cherry picked from commit 2a783b046769fb5871a62ed2bd5e7ceca6296803)

--- hal/msm8974/platform.c
+++ hal/msm8974/platform.c
@@ -7667,7 +7667,8 @@ snd_device_t platform_get_input_snd_device(void *platform,
                                  : (my_data->fluence_nn_enabled ?
                                         SND_DEVICE_IN_HANDSET_MIC_NN
                                         : SND_DEVICE_IN_HANDSET_MIC);
-                 if (audio_extn_hfp_is_active(adev))
+                 if (audio_extn_hfp_is_active(adev) &&
+                     !(in->enable_aec || in->enable_ec_port))
                      platform_set_echo_reference(adev, true, out_devices);
             } else {
                 if ((my_data->fluence_type & FLUENCE_TRI_MIC) &&
@@ -7687,7 +7688,8 @@ snd_device_t platform_get_input_snd_device(void *platform,
             }
         } else if (compare_device_type(out_devices, AUDIO_DEVICE_OUT_WIRED_HEADSET)) {
             snd_device = SND_DEVICE_IN_VOICE_HEADSET_MIC;
-            if (audio_extn_hfp_is_active(adev))
+            if (audio_extn_hfp_is_active(adev) &&
+                !(in->enable_aec || in->enable_ec_port))
                 platform_set_echo_reference(adev, true, out_devices);
         } else if (is_sco_out_device_type(out_devices)) {
             if (adev->swb_speech_mode != SPEECH_MODE_INVALID) {
@@ -7715,7 +7717,8 @@ snd_device_t platform_get_input_snd_device(void *platform,
                                 SND_DEVICE_IN_HANDSET_MIC_SB
                                  : SND_DEVICE_IN_HANDSET_MIC;
 
-            if (voice_is_in_call(adev))
+            if (voice_is_in_call(adev) &&
+                !(in->enable_aec || in->enable_ec_port))
                 platform_set_echo_reference(adev, true, out_devices);
         } else if (compare_device_type(out_devices, AUDIO_DEVICE_OUT_SPEAKER) ||
                    compare_device_type(out_devices, AUDIO_DEVICE_OUT_SPEAKER_SAFE) ||
@@ -7742,19 +7745,22 @@ snd_device_t platform_get_input_snd_device(void *platform,
                                         SND_DEVICE_IN_VOICE_SPEAKER_DMIC_SB
                                         : SND_DEVICE_IN_VOICE_SPEAKER_DMIC;
                 }
-                if (audio_extn_hfp_is_active(adev))
+                if (audio_extn_hfp_is_active(adev) &&
+                    !(in->enable_aec || in->enable_ec_port))
                     platform_set_echo_reference(adev, true, out_devices);
             } else {
                 if (adev->enable_hfp) {
                     snd_device = SND_DEVICE_IN_VOICE_SPEAKER_MIC_HFP;
-                    platform_set_echo_reference(adev, true, out_devices);
+                    if (!(in->enable_aec || in->enable_ec_port))
+                        platform_set_echo_reference(adev, true, out_devices);
                 } else {
                     snd_device = my_data->fluence_sb_enabled ?
                                      SND_DEVICE_IN_VOICE_SPEAKER_MIC_SB
                                      : (my_data->fluence_nn_enabled ?
                                          SND_DEVICE_IN_VOICE_SPEAKER_MIC_NN
                                          : SND_DEVICE_IN_VOICE_SPEAKER_MIC);
-                    if (audio_extn_hfp_is_active(adev))
+                    if (audio_extn_hfp_is_active(adev) &&
+                        !(in->enable_aec || in->enable_ec_port))
                         platform_set_echo_reference(adev, true, out_devices);
                 }
             }
